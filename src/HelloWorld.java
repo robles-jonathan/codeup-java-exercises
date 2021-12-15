eb2b4d0273e009f3ba509a0137c46faad207e934
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-15 10:13:23 -0600

    walk thru on exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,12 +17,18 @@ public class HelloWorld {
         //Declare an long variable named myNumber, but do not assign anything to it. Next try to print out myNumber to the console. What happens? myNumber has not been initialized
 
         //Change your code to assign the value 3.14 to myNumber. What do you notice? Will not work, double provided not Long
-        float myNumber = 3.14F;
+//        float myNumber = 3.14F;
+        //or
+        float myNumber = (float)3.14;
         System.out.println(myNumber);
 
         //Why does assigning the value 3.14 to a variable declared as a long not compile, but assigning an integer value does? Because Long does not recognize decimal
 
-        //Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this? Add F to the end of the number.
+        //Change your code to declare myNumber as a float. Assign the value 3.14 to it. What happens? What are two ways we could fix this?
+        // We can add F literal notation to the end of the number.
+        // ex. float myNumber = 3.14F;
+        // Or we can cast to a float.
+        // ex. float myNumber = (float)3.14;
         int x = 5;
         System.out.println(x++);
         System.out.println(x);
@@ -71,6 +77,12 @@ public class HelloWorld {
         e -= d;
         System.out.println("10/2 = " + d);
         System.out.println("2-10 = " + e);
+
+        //What happens if you assign a value to a numerical variable that is larger (or smaller) than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+        //
+        //Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
+//        byte w = 129;
+        System.out.println(Byte.MAX_VALUE);
 
         //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~LECTURE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         int month = 12;
