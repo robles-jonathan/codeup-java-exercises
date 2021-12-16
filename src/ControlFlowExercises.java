ab9320acbe18d04220929674ce310b5c32a77bfe
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-16 15:44:00 -0600

    3 complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,18 +3,24 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     private static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~FIZZBUZZ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-        for (int i = 1; i <= 100; i++){
-            if(i % 15 == 0){
-                System.out.println("FizzBuzz");
-            }else if (i % 5 == 0){
-                System.out.println("Buzz");
-            }else if (i % 3 == 0){
-                System.out.println("Fizz");
-            }else {
-                System.out.println(i);
+
+        String response;
+        do{
+            System.out.println("What number would you like to go up to?");
+            Scanner input = new Scanner(System.in);
+            int num = input.nextInt();
+            System.out.println("Here is your table");
+            System.out.println("NUMBER | SQUARE | CUBED ");
+            System.out.println("-------|--------|-------");
+            for (int i = 1; i <= num; i++) {
+                int num1 = i;
+                int num2 = (int) Math.pow(i, 2);
+                int num3 = (int) Math.pow(i, 3);
+                System.out.printf("%-6d | %-6d | %-6d%n", num1, num2, num3);
             }
-        }
+            System.out.println("Would you like to continue?");
+            response = sc.next();
+        } while(response.equalsIgnoreCase("yes"));
     }
 
 }
