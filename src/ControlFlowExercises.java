5e9ac5c3b47e35f2688bcc9f7eb676a51a9d8d73
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-16 13:51:14 -0600

    1.a complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     private static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+        //While loop
+        int i = 5;
+        while (i <= 15) {
+            System.out.println(i++);
+        }
     }
 }
