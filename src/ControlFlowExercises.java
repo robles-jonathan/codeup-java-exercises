f33865e13b4d7919d00fad16568435df18a9932c
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-16 13:51:53 -0600

    1.b.a complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     private static Scanner sc = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         //While loop
@@ -10,5 +9,11 @@ public class ControlFlowExercises {
         while (i <= 15) {
             System.out.println(i++);
         }
+        //Do While loop
+        int a = 2;
+        do{
+            System.out.println(a);
+            a += 2;
+        } while(a <= 100);
     }
 }
