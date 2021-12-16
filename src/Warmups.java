6387d9a1cf967173d731391555614371e4b2a582
Jonathan Robles <jonathan.robles287@gmail.com>
2021-12-16 09:10:22 -0600

    Finished morning warm up 12/16/21

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Warmups {
     public static void main (String[] args){
         System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+        System.out.println("What did you eat for breakfast?");
+        Scanner scanner = new Scanner(System.in);
+        String userBreakfast = scanner.nextLine();
+        System.out.printf("OH Wow!!, you had %s for breakfast that's my favorite!",userBreakfast);
+
     }
 }
