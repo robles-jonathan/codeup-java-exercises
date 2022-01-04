8d6ab63f4c5c7725b59d869105809ab1f64a1311
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-04 09:44:25 -0600

    Walk thru of warm up complete & Methods Exercises Added

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 
 public class Warmups {
+    //Warmup 1-4-22
+    public static String startDay(){
+        return "Brain loading... Done!";
+    }
     public static void main (String[] args){
-        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
-        System.out.println("What did you eat for breakfast?");
-        Scanner scanner = new Scanner(System.in);
-        String userBreakfast = scanner.nextLine();
-        System.out.printf("OH Wow!!, you had %s for breakfast. My favorite breakfast is %s!", userBreakfast, userBreakfast);
+//        System.out.println("Java is statically typed, while Javascript is dynamically typed.");
+//        System.out.println("What did you eat for breakfast?");
+//        Scanner scanner = new Scanner(System.in);
+//        String userBreakfast = scanner.nextLine();
+//        System.out.printf("OH Wow!!, you had %s for breakfast. My favorite breakfast is %s!", userBreakfast, userBreakfast);
+
+        //Warmup 1-4-22
+        System.out.println(startDay());
     }
 }
