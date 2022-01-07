111f2d2b2d0dbb5aac5930ed93a2012427d035aa
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-07 16:46:33 -0600

    Created ShapesTests Class

---
+++ src/shapes/Square.java
@@ -0,0 +1,14 @@
+package shapes;
+
+public class Square extends Rectangle {
+    
+    //Empty Constructor
+    public Square(){
+    }
+
+    //Constructor with one Arg
+    public Square(int side){
+        super(side, side);
+    }
+
+}
