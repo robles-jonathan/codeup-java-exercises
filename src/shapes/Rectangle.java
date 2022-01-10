5ac2c727dfda568fc3c7105a6c3f350804e8839e
Jonathan Robles <jonathan.robles287@gmail.com>
2022-01-10 12:35:21 -0600

    finished

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,29 +1,53 @@
 package shapes;
 
-public class Rectangle {
-    protected int length;
-    protected int width;
-
-    //Empty Constructor
-    public Rectangle(){
+public class Rectangle extends Quadrilateral implements Measurable{
+    public Rectangle(double length, double width){
+        super(length, width);
     }
 
-    //Constructor with two Args
-    public Rectangle(int length, int width){
-        this.length = length;
-        this.width = width;
-    }
 
-    public int getArea(){
-        return length * width;
-    }
-
-    public int getPerimeter(){
+    @Override
+    public double getPerimeter() {
         return 2 * length + 2 * width;
     }
 
-    public void sayHello(){
-        System.out.println("Hello, World!");
+    @Override
+    public double getArea() {
+        return length * width;
     }
+
+    @Override
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
+        this.width = width;
+    }
+//    protected int length;
+//    protected int width;
+//
+//    //Empty Constructor
+//    public Rectangle(){
+//    }
+//
+//    //Constructor with two Args
+//    public Rectangle(int length, int width){
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public int getArea(){
+//        return length * width;
+//    }
+//
+//    public int getPerimeter(){
+//        return 2 * length + 2 * width;
+//    }
+//
+//    public void sayHello(){
+//        System.out.println("Hello, World!");
+//    }
 
 }
